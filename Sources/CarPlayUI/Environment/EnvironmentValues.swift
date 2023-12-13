205253939d523ec3d5f45ed1759c1dc9a4515c3a
Alsey Coleman Miller <alseycmiller@gmail.com>
2023-12-12 23:06:04 -0500

    Add `EnvironmentValues`

---
+++ Sources/CarPlayUI/Environment/EnvironmentValues.swift
@@ -0,0 +1,88 @@
+//
+//  Transaction.swift
+//
+//
+//  Created by Alsey Coleman Miller on 12/11/23.
+//
+
+import Combine
+
+public struct EnvironmentValues: CustomStringConvertible {
+  public var description: String {
+    "EnvironmentValues: \(values.count)"
+  }
+
+  private var values: [ObjectIdentifier: Any] = [:]
+
+  public init() {}
+
+  public subscript<K>(key: K.Type) -> K.Value where K: EnvironmentKey {
+    get {
+      if let val = values[ObjectIdentifier(key)] as? K.Value {
+        return val
+      }
+      return K.defaultValue
+    }
+    set {
+      values[ObjectIdentifier(key)] = newValue
+    }
+  }
+
+  subscript<B>(bindable: ObjectIdentifier) -> B? where B: ObservableObject {
+    get {
+      values[bindable] as? B
+    }
+    set {
+      values[bindable] = newValue
+    }
+  }
+
+  @_spi(CarPlayUI)
+  public mutating func merge(_ other: Self?) {
+    if let other = other {
+      values.merge(other.values) { _, new in
+        new
+      }
+    }
+  }
+
+  @_spi(CarPlayUI)
+  public func merging(_ other: Self?) -> Self {
+    var merged = self
+    merged.merge(other)
+    return merged
+  }
+}
+
+struct IsEnabledKey: EnvironmentKey {
+  static let defaultValue = true
+}
+
+public extension EnvironmentValues {
+  var isEnabled: Bool {
+    get {
+      self[IsEnabledKey.self]
+    }
+    set {
+      self[IsEnabledKey.self] = newValue
+    }
+  }
+}
+
+struct _EnvironmentValuesWritingModifier: ViewModifier, _EnvironmentModifier {
+  let environmentValues: EnvironmentValues
+
+  func body(content: Content) -> some View {
+    content
+  }
+
+  func modifyEnvironment(_ values: inout EnvironmentValues) {
+    values = environmentValues
+  }
+}
+
+public extension View {
+  func environmentValues(_ values: EnvironmentValues) -> some View {
+    modifier(_EnvironmentValuesWritingModifier(environmentValues: values))
+  }
+}
