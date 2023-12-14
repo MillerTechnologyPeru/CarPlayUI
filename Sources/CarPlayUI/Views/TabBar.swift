f755a2216264bab78c785feb679310b5f3d0e72b
Alsey Coleman Miller <alseycmiller@gmail.com>
2023-12-14 08:43:32 -0500

    Add `TabView`

---
+++ Sources/CarPlayUI/Views/TabBar.swift
@@ -0,0 +1,49 @@
+//
+//  TabBar.swift
+//
+//
+//  Created by Alsey Coleman Miller on 12/14/23.
+//
+
+import Foundation
+import CarPlay
+import TokamakCore
+
+import Foundation
+import CarPlay
+import TokamakCore
+
+@available(iOS 14.0, *)
+public struct TabView <Content: View> : View {
+    
+    let content: Content
+    
+    public init(
+        @ViewBuilder content: () -> Content
+    ) {
+        self.content = content()
+    }
+    
+    public var body: Content {
+        content
+    }
+}
+
+@available(iOS 14.0, *)
+extension TabView: CarPlayPrimitive {
+    
+    @_spi(TokamakCore)
+    public var renderedBody: AnyView {
+        AnyView(
+            TemplateView(
+                build: { scene in
+                    CPTabBarTemplate(templates: [])
+                },
+                update: { (target, scene) in
+                    
+                },
+                content: { content }
+            )
+        )
+    }
+}
