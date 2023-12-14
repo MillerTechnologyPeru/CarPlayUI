bd3a5a9ea276e9b4bd8cef0237959f372d24fc1a
Alsey Coleman Miller <alseycmiller@gmail.com>
2023-12-14 08:41:42 -0500

    Add `ButtonRole.confirm`

--- Sources/CarPlayUI/Views/Button.swift
+++ Sources/CarPlayUI/Views/Button.swift
@@ -9,16 +9,21 @@ import Foundation
 import CarPlay
 import TokamakCore
 
+public extension ButtonRole {
+    
+    static var confirm: ButtonRole { ButtonRole(rawValue: 2) }
+}
+
 @available(iOS 14.0, *)
 protocol TextButton {
     
-    var textButton: CPTextButton { get }
+    func textButton(for scene: CPTemplateApplicationScene) -> CPTextButton
 }
 
 @available(iOS 14.0, *)
 extension Button: TextButton where Label == Text {
     
-    var textButton: CPTextButton {
+    func textButton(for scene: CPTemplateApplicationScene) -> CPTextButton {
         CPTextButton(
             title: _TextProxy(label).rawText,
             textStyle: .init(role: role),
@@ -32,12 +37,26 @@ extension Button: TextButton where Label == Text {
 @available(iOS 14.0, *)
 extension _Button: TextButton where Label == Text {
     
-    var textButton: CPTextButton {
+    func textButton(for scene: CPTemplateApplicationScene) -> CPTextButton {
         CPTextButton(
             title: _TextProxy(label).rawText,
             textStyle: .init(role: role),
             handler: { _ in
                 action()
+            }
+        )
+    }
+}
+
+@available(iOS 14.0, *)
+extension NavigationLink: TextButton where Label == Text {
+    
+    func textButton(for scene: CPTemplateApplicationScene) -> CPTextButton {
+        CPTextButton(
+            title: _TextProxy(label).rawText,
+            textStyle: .normal,
+            handler: { _ in
+                // TODO: Navigation
             }
         )
     }
@@ -54,6 +73,8 @@ internal extension CPTextButtonStyle {
         switch role {
         case .cancel:
             self = .cancel
+        case .confirm:
+            self = .confirm
         case .destructive:
             self = .confirm
         default:
