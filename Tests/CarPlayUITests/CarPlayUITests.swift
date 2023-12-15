cfe7ad20b2b14432fde849e3c5d9b8a0eb7d1d6a
Alsey Coleman Miller <alseycmiller@gmail.com>
2023-12-15 06:32:10 -0500

    Updated unit tests

--- Tests/CarPlayUITests/CarPlayUITests.swift
+++ Tests/CarPlayUITests/CarPlayUITests.swift
@@ -5,7 +5,17 @@ import CarPlay
 
 final class CarPlayUITests: XCTestCase {
     
-    func testTemplateState() throws {
+    func testInsertBefore() throws {
         
+        let values: [(String, Int, [String], [String])] = [
+            ("New Item", 0, ["1", "2", "3"], ["New Item", "1", "2", "3"]),
+            ("New Item", 1, ["1", "2", "3"], ["1", "New Item", "2", "3"]),
+            ("New Item", 2, ["1", "2", "3"], ["1", "2", "New Item", "3"])
+        ]
+        for (newValue, index, oldValue, expectedValue) in values {
+            var result = oldValue
+            result.insert(newValue, before: index)
+            XCTAssertEqual(result, expectedValue)
+        }
     }
 }
