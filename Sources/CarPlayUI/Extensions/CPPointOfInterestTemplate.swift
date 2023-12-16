ca6bf3f9e202b882b2f425e9046ed60e02fec4b5
Alsey Coleman Miller <alseycmiller@gmail.com>
2023-12-16 18:42:48 -0500

    Add `CPPointOfInterestTemplate` extensions

---
+++ Sources/CarPlayUI/Extensions/CPPointOfInterestTemplate.swift
@@ -0,0 +1,51 @@
+//
+//  CPPointOfInterestTemplate.swift
+//
+//
+//  Created by Alsey Coleman Miller on 12/16/23.
+//
+
+import Foundation
+import CarPlay
+
+@available(iOS 14.0, *)
+internal extension CPPointOfInterestTemplate {
+    
+    func insert(_ item: CPPointOfInterest, before sibling: CPPointOfInterest? = nil) {
+        var items = self.pointsOfInterest
+        // move to before sibling
+        if let sibling, let index = items.firstIndex(of: sibling) {
+            items.insert(item, before: index)
+        } else {
+            // append to end
+            items.append(item)
+        }
+        // update points of interest
+        setPointsOfInterest(items, selectedIndex: selectedIndex)
+    }
+    
+    func update(
+        oldValue: CPPointOfInterest,
+        newValue: CPPointOfInterest
+    ) {
+        var items = self.pointsOfInterest
+        guard let index = items.firstIndex(where: { $0 === oldValue }) else {
+            assertionFailure("Unable to find item in graph")
+            return
+        }
+        // update with new instance at
+        items[index] = newValue
+        // update points of interest
+        setPointsOfInterest(items, selectedIndex: selectedIndex)
+    }
+    
+    func remove(item: CPInformationItem) {
+        var items = self.pointsOfInterest
+        guard let index = items.firstIndex(where: { $0 === item }) else {
+            return
+        }
+        items.remove(at: index)
+        // update points of interest
+        setPointsOfInterest(items, selectedIndex: selectedIndex)
+    }
+}
