2b12cba8472a5b7069c6a9150b80d9b57b4e2eb0
Alsey Coleman Miller <alseycmiller@gmail.com>
2023-12-16 18:42:35 -0500

    Add `Map.Template`

--- Sources/CarPlayUI/Views/MapView.swift
+++ Sources/CarPlayUI/Views/MapView.swift
@@ -11,134 +11,146 @@ import MapKit
 import CarPlay
 
 /// Shows points of interest on CarPlay.
-public struct Map: View {
+@available(iOS 14.0, *)
+public struct Map <Content: View>: View {
     
-    @Binding
-    var region: MKCoordinateRegion
+    public typealias SelectionValue = Int
     
-    @Binding
-    var selection: Int?
+    let region: Binding<MKCoordinateRegion>?
+    
+    let selection: Binding<Int?>?
+    
+    let content: Content
+    
+    public init(
+        region: Binding<MKCoordinateRegion>? = nil,
+        selection: Binding<SelectionValue?>? = nil,
+        @ViewBuilder content: () -> Content
+    ) {
+        self.region = region
+        self.selection = selection
+        self.content = content()
+    }
     
     public var body: some View {
-        EmptyView()
+        ToolbarReader { (title, toolbar) in
+            Template(
+                title: title.flatMap { mapAnyView($0, transform: { (view: Text) in _TextProxy(view).rawText }) } ?? "",
+                region: region,
+                selection: selection,
+                content: content
+            )
+        }
     }
 }
 
-/// Point of Interest to show on the map.
-public struct Annotation<Label, Content> where Label : View, Content : View {
-    
-    init(
-        location: MKMapItem,
-        title: String,
-        subtitle: String?,
-        summary: String?,
-        detailTitle: String?,
-        detailSubtitle: String?,
-        detailSummary: String?,
-        pinImage: UIImage?,
-        selectedPinImage: UIImage?
-    ) {
-        
-    }
-    
-    public var body: some View {
-        EmptyView()
-    }
-}
+// MARK: - CarPlayPrimitive
 
 @available(iOS 14.0, *)
-public extension Map {
+extension Map {
     
-    final class Coordinator: NSObject, NavigationStackTemplateCoordinator, CPPointOfInterestTemplateDelegate {
+    struct Template: View {
         
-        @Binding
-        var region: MKCoordinateRegion
+        let title: String
         
-        @Binding
-        var selection: Int?
+        let region: Binding<MKCoordinateRegion>?
+        
+        let selection: Binding<Int?>?
+        
+        let content: Content
+        
+        public var body: Content {
+            content
+        }
+    }
+}
+
+
+@available(iOS 14.0, *)
+extension Map.Template: CarPlayPrimitive {
+    
+    public var renderedBody: AnyView {
+        AnyView(
+            TemplateView(
+                build: {
+                    let coordinator = CPPointOfInterestTemplate.Coordinator(
+                        region: self.region,
+                        selection: self.selection
+                    )
+                    let template = CPPointOfInterestTemplate(
+                        title: title,
+                        pointsOfInterest: [],
+                        selectedIndex: NSNotFound
+                    )
+                    template.userInfo = coordinator
+                    return template
+                },
+                update: { (template: CPPointOfInterestTemplate) in
+                    // update title
+                    if template.title != title {
+                        template.title = title
+                    }
+                    // update selection
+                    if let selection,
+                       selection.wrappedValue.toFoundation() != template.selectedIndex,
+                       selection.wrappedValue.toFoundation() < template.pointsOfInterest.count {
+                        let newIndex = selection.wrappedValue.toFoundation()
+                        template.setPointsOfInterest(template.pointsOfInterest, selectedIndex: newIndex)
+                    }
+                },
+                content: { content }
+            )
+        )
+    }
+}
+
+// MARK: - Coordinator
+
+@available(iOS 14.0, *)
+public extension CPPointOfInterestTemplate {
+    
+    final class Coordinator: NSObject, NavigationStackTemplateCoordinator {
+        
+        let region: Binding<MKCoordinateRegion>?
+        
+        let selection: Binding<Int?>?
         
         var navigationDestination: NavigationDestination?
         
         var navigationContext: NavigationContext?
         
-        fileprivate init(region: Binding<MKCoordinateRegion>, selection: Binding<Int?>) {
-            _region = region
-            _selection = selection
+        fileprivate init(
+            region: Binding<MKCoordinateRegion>?,
+            selection: Binding<Int?>?
+        ) {
+            self.region = region
+            self.selection = selection
             super.init()
         }
-        
-        /**
-         The user has changed the map region on the `CPPointOfInterestTemplate`. Your application
-         should respond by updating `pointsOfInterest` to show new points of interest for the new region.
-         */
-        public func pointOfInterestTemplate(_ pointOfInterestTemplate: CPPointOfInterestTemplate, didChangeMapRegion region: MKCoordinateRegion) {
-            self.region = region
-        }
-        
-        /**
-         The user has selected the `pointOfInterest` and the details are being shown.
-         */
-        public func pointOfInterestTemplate(_ pointOfInterestTemplate: CPPointOfInterestTemplate, didSelectPointOfInterest pointOfInterest: CPPointOfInterest) {
-            
-        }
     }
 }
 
-// MARK: - Button
+// MARK: - CPPointOfInterestTemplateDelegate
 
 @available(iOS 14.0, *)
-internal extension Button where Label == Text {
+extension CPPointOfInterestTemplate.Coordinator: CPPointOfInterestTemplateDelegate {
     
-    func build(pointOfInterest: CPPointOfInterest, before sibling: CPTextButton? = nil) -> CPTextButton {
-        let newButton = buildTextButton()
-        pointOfInterest.insert(newButton, before: sibling)
-        return newButton
+    /**
+     The user has changed the map region on the `CPPointOfInterestTemplate`. Your application
+     should respond by updating `pointsOfInterest` to show new points of interest for the new region.
+     */
+    public func pointOfInterestTemplate(_ pointOfInterestTemplate: CPPointOfInterestTemplate, didChangeMapRegion region: MKCoordinateRegion) {
+        
+        // update region
+        self.region?.wrappedValue = region
     }
     
-    func update(_ oldValue: CPTextButton, pointOfInterest: CPPointOfInterest) -> CPTextButton {
-        let newValue = buildTextButton()
-        pointOfInterest.update(oldValue: oldValue, newValue: newValue)
-        return newValue
-    }
-}
-
-@available(iOS 14.0, *)
-internal extension CPPointOfInterest {
-    
-    var buttons: [CPTextButton] {
-        get {
-            [primaryButton, secondaryButton].compactMap { $0 }
-        }
-        set {
-            assert(newValue.count <= 2, "Can add a maximum of 2 buttons")
-            primaryButton = newValue.count > 0 ? newValue[0] : nil
-            secondaryButton = newValue.count > 1 ? newValue[1] : nil
-        }
-    }
-    
-    func insert(_ button: CPTextButton, before sibling: CPTextButton? = nil) {
-        // move to before sibling
-        if let sibling, let index = buttons.firstIndex(of: sibling) {
-            buttons.insert(button, before: index)
-        } else {
-            // append to end
-            buttons.append(button)
-        }
-    }
-    
-    func update(oldValue: CPTextButton, newValue: CPTextButton) {
-        guard let index = buttons.firstIndex(where: { $0 === oldValue }) else {
-            assertionFailure("Unable to find item in graph")
-            return
-        }
-        // update with new instance at index
-        buttons[index] = newValue
-    }
-    
-    func remove(button: CPTextButton) {
-        guard let index = buttons.firstIndex(where: { $0 === button }) else {
-            return
-        }
-        buttons.remove(at: index)
+    /**
+     The user has selected the `pointOfInterest` and the details are being shown.
+     */
+    public func pointOfInterestTemplate(_ pointOfInterestTemplate: CPPointOfInterestTemplate, didSelectPointOfInterest pointOfInterest: CPPointOfInterest) {
+        
+        // update selection index
+        self.selection?.wrappedValue = pointOfInterestTemplate.selectedIndex.filterNSNotFound()
     }
 }
